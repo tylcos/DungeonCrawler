6063d74f0418efa767797cde131d089b0f4ade1a
Krishan Patel <krishanpatel00@gmail.com>
2021-02-19 18:41:39 -0500

    first commit to test checkstyle

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+    /**
+     * Bootstraps main application and runs the main window class
+     * @param args CLI args (ignored)
+     */
+    public static void main(String[] args) {
+        CalculatorWindow window = new CalculatorWindow();
+        window.show();
+    }
+}
