51c419b71a7d1c0b4021bb700f8ae3c7f45a29fc
krishan patel <kpatel648@gatech.edu>
2021-02-24 23:23:36 -0500

    update file structure and add junit tests

--- src/test/java/DungeonCrawlerTest.java
+++ src/test/java/DungeonCrawlerTest.java
@@ -1,7 +1,27 @@
+import javafx.stage.Stage;
 import org.junit.Test;
-import org.junit.Assert;
 import dungeonUI.DungeonCrawlerDriver;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
 
-public class DungeonCrawlerTest {
+import static org.testfx.api.FxAssert.verifyThat;
 
+public class DungeonCrawlerTest extends ApplicationTest {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        DungeonCrawlerDriver dcd = new DungeonCrawlerDriver();
+        dcd.start(primaryStage);
+    }
+
+    @Test
+    public void testStartNameFieldExists() {
+        clickOn("Start");
+        verifyThat("Name", NodeMatchers.isNotNull());
+    }
+
+    @Test
+    public void testStartDifficultyFieldExists() {
+        clickOn("Start");
+        verifyThat("Difficulty", NodeMatchers.isNotNull());
+    }
 }
