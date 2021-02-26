e9624ab352ff500e36eb742a98c1491df4dd6321
cKompella <chinmayi.kompella@gmail.com>
2021-02-25 22:58:01 -0500

    add invalidname and name population tests

--- src/test/java/DungeonCrawlerTest.java
+++ src/test/java/DungeonCrawlerTest.java
@@ -1,18 +1,21 @@
-import javafx.stage.Stage;
 import org.junit.Test;
 import driver.DungeonCrawlerDriver;
+import org.testfx.api.FxRobotException;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
-
+import org.testfx.matcher.control.LabeledMatchers;
 import static org.testfx.api.FxAssert.verifyThat;
 
+import javafx.stage.Stage;
+
 public class DungeonCrawlerTest extends ApplicationTest {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         DungeonCrawlerDriver dcd = new DungeonCrawlerDriver();
         dcd.start(primaryStage);
     }
-
+    
     @Test
     public void testStartNameFieldExists() {
         clickOn("Start");
@@ -23,5 +26,35 @@ public class DungeonCrawlerTest extends ApplicationTest {
     public void testStartDifficultyFieldExists() {
         clickOn("Start");
         verifyThat("Difficulty", NodeMatchers.isNotNull());
+    }
+
+    @Test
+    public void testStartWeaponFieldExists() {
+        clickOn("Start");
+       verifyThat("Weapon", NodeMatchers.isNotNull());
+    }
+
+    //throws this exception when node is not found
+    @Test(expected = FxRobotException.class)
+    public void testEmptyName() {
+        clickOn("Start");
+        clickOn("Go to first room");
+        clickOn("Shop");
+    }
+
+    @Test(expected = FxRobotException.class)
+    public void testWhitespaceName() {
+        clickOn("Start");
+        clickOn(".text").write(" ");
+        clickOn("Go to first room");
+        clickOn("Shop");
+    }
+
+    @Test
+    public void testPlayerNamePopulation() {
+        clickOn("Start");
+        clickOn(".text").write("Azula");
+        clickOn("Go to first room");
+        verifyThat(".label", LabeledMatchers.hasText("Name: Azula"));
     }
 }
