4cddd80e19e9d3f71c94757f3d3b8677b213728d
LoftedSpy <LoftedSpy@users.noreply.github.com>
2021-03-02 17:54:51 -0500

    Make checkstyle happy

--- src/main/java/game/Room.java
+++ src/main/java/game/Room.java
@@ -15,44 +15,56 @@ import javafx.scene.layout.StackPane;
 /**
  * Room is a GridPane that displays a given room as a grid of Labels.
  * 
- * Room also contains all data relevant to a room, like the items and enemies inside of it.
+ * Room also contains all data relevant to a room, like the items and enemies
+ * inside of it.
  * 
- * !!! THIS HAS ONLY BEEN TESTED ON RECTANGULAR ROOMS. DO NOT ATTEMPT TO CREATE NON-RECTANGULAR ROOMS !!!
+ * !!! THIS HAS ONLY BEEN TESTED ON RECTANGULAR ROOMS. DO NOT ATTEMPT TO CREATE
+ * NON-RECTANGULAR ROOMS !!!
  */
 public class Room extends GridPane {
-    
-    // A lookup table to translate between letters in .room files and sprites in game
-    private static Hashtable<String, Image> spriteTable = new Hashtable<String, Image>() 
-    {{  put(".", new Image(Room.class.getResource("/images/debug square.png").toString()));
-        put("x", new Image(Room.class.getResource("/images/debug wall.png").toString()));
-        put("D", new Image(Room.class.getResource("/images/debug door.png").toString()));   }};
-    
-    // A cache for all rooms that have been loaded before so that we don't have to read from disk every time.
-    // I have not tested this at all and I don't even know if the performance is worth the memory or if this even
-    // takes up a lot of memory, but its an idea anyway. Actually, this thing isn't even implemented soooo
+
+    // A lookup table to translate between letters in .room files and sprites in
+    // game
+    private static Hashtable<String, Image> spriteTable = new Hashtable<String, Image>() {
+        {
+            put(".", new Image(Room.class.getResource("/images/debug square.png").toString()));
+            put("x", new Image(Room.class.getResource("/images/debug wall.png").toString()));
+            put("D", new Image(Room.class.getResource("/images/debug door.png").toString()));
+        }
+    };
+
+    // A cache for all rooms that have been loaded before so that we don't have to
+    // read from disk every time.
+    // I have not tested this at all and I don't even know if the performance is
+    // worth the memory or if this even
+    // takes up a lot of memory, but its an idea anyway. Actually, this thing isn't
+    // even implemented soooo
     // TODO implement or remove room cache
     private static Hashtable<String, GridPane> roomCache = new Hashtable<String, GridPane>();
 
     // A list of all collidable bodies making up this room
-    ArrayList<Collidable> bodies = new ArrayList<Collidable>();
+    private ArrayList<Collidable> bodies = new ArrayList<Collidable>();
     /*
-     * Room will also hold data on the locations of items and enemies inside of it for Level to place into layers
-     * Level will only keep one room loaded at a time, so this will allow for object permanance
+     * Room will also hold data on the locations of items and enemies inside of it
+     * for Level to place into layers Level will only keep one room loaded at a
+     * time, so this will allow for object permanance
      */
-    
+
     /**
      * Constructs a room from a premade blueprint file.
+     * 
      * @param template the file to load the room from
      */
     public Room(String template) {
         super();
-        setAlignment(Pos.CENTER); // By default GridPanes are aligned to the top-left, which we don't want
-        
+        // By default GridPanes are aligned to the top-left, which we don't want
+        setAlignment(Pos.CENTER);
+
         GridPane cache = roomCache.get(template);
         if (cache != null) {
             // Pull up room from cache
         }
-        
+
         // Read the file
         InputStream is = Room.class.getResourceAsStream(template);
         if (is == null) {
@@ -68,7 +80,7 @@ public class Room extends GridPane {
             System.err.println("Error occurred trying to read from " + template);
             e.printStackTrace();
         }
-        
+
         // Place the tiles
         for (int row = 0; row < lines.size(); ++row) {
             for (int col = 0; col < lines.get(row).length(); ++col) {
@@ -77,13 +89,14 @@ public class Room extends GridPane {
                 }
                 Image img = spriteTable.get("" + lines.get(row).charAt(col));
                 if (img == null) {
-                    System.err.println("Detected an invalid character in room " + template + "! Please check file for errors!");
+                    System.err.println("Detected an invalid character in room " + template
+                            + "! Please check file for errors!");
                 }
 
                 StackPane cell = new StackPane();
                 cell.setMaxSize(100.0, 100.0);
                 cell.setMinSize(100.0, 100.0);
-                switch(lines.get(row).charAt(col)) {
+                switch (lines.get(row).charAt(col)) {
                 case 'D':
                     Door d = new Door(img, true);
                     bodies.add(d);
@@ -102,8 +115,13 @@ public class Room extends GridPane {
             }
         }
         if (cache != null) {
-            roomCache.put(template, (GridPane) this); // This would probably be better if we cloned it instead of copying a reference...
+            // This would probably be better if we cloned it instead of copying a
+            // reference...
+            roomCache.put(template, (GridPane) this);
         }
     }
 
+    public ArrayList<Collidable> getBodies() {
+        return bodies;
+    }
 }
