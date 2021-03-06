d110a0140f72e4e27e8d708d48396a27d8452e35
tylcos <tylcos@users.noreply.github.com>
2021-03-06 10:13:45 -0500

    Add parameter --scene=name for skipping title screen
    Typical usage is: gradle run --args='--scene=GAME'

--- src/main/java/core/DungeonCrawlerDriver.java
+++ src/main/java/core/DungeonCrawlerDriver.java
@@ -1,7 +1,10 @@
 package core;
 
+import game.MainPlayer;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.util.Map;
 
 /**
  * Launches application
@@ -13,6 +16,25 @@ public class DungeonCrawlerDriver extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        core.SceneManager.loadStage();
+        Map<String, String> parameters = getParameters().getNamed();
+
+        String requestedScene = parameters.getOrDefault("scene", "");
+        if (!requestedScene.isEmpty()) {
+            System.out.println("Loading scene: " + requestedScene);
+            try {
+                String scene = SceneManager.class.getField(requestedScene).get(null).toString();
+
+                SceneManager.loadStage();
+                SceneManager.loadScene(scene);
+                GameManager.setPlayer(new MainPlayer("Azula", "Weapon", "Normal"));
+                return;
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                System.err.println("Could not load scene: " + requestedScene + " requested by "
+                                           + "parameter --scene");
+                e.printStackTrace();
+            }
+        }
+
+        SceneManager.loadStage();
     }
 }
