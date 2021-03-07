1edeff48102e586d1a1e34769c50375c3a55e0a9
tylcos <tylcos@users.noreply.github.com>
2021-03-07 13:57:24 -0500

    Clean up files and change Level to allow spawning enemies

--- src/main/java/game/Door.java
+++ src/main/java/game/Door.java
@@ -4,7 +4,6 @@ import core.GameManager;
 import javafx.scene.image.Image;
 
 public class Door extends CollidableTile {
-
     private Room destination;
 
     public Door(Image image, boolean isStatic) {
@@ -14,16 +13,17 @@ public class Door extends CollidableTile {
     public Door(Image image, boolean isStatic, Room destination) {
         super(image, isStatic);
 
-        this.setDestination(destination);
+        this.destination = destination;
     }
 
     @Override
     public void onCollision(Collidable other) {
-        GameManager.getLevel().setRoom(destination);
+        if (other instanceof MainPlayer) {
+            GameManager.getLevel().setRoom(destination);
+        }
     }
 
     public void setDestination(Room destination) {
         this.destination = destination;
     }
-
 }
