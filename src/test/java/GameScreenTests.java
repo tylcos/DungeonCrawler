8dec346e44ce4d65ea6d6880c48a1680eda2a74d
cKompella <cKompella@users.noreply.github.com>
2021-03-12 13:09:36 -0500

    add test cases

--- src/test/java/GameScreenTests.java
+++ src/test/java/GameScreenTests.java
@@ -1,6 +1,13 @@
 import core.DungeonCrawlerDriver;
+import core.GameManager;
 import core.SceneManager;
+import game.Door;
+import game.MainPlayer;
+import game.Room;
+import javafx.scene.input.KeyCode;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
 import static org.junit.Assert.assertEquals;
@@ -13,4 +20,42 @@ public class GameScreenTests extends ApplicationTest {
         launch(DungeonCrawlerDriver.class, "--scene=GAME");
         assertEquals(SceneManager.getSceneName(), SceneManager.GAME);
     }
+
+    @Test
+    public void testMoveLeft() {
+        double initialX = GameManager.getPlayer().getX();
+        press(KeyCode.LEFT);
+        double finalX = GameManager.getPlayer().getX();
+        Assert.assertTrue(finalX < initialX);
+    }
+
+    @Test
+    public void testMoveRight(){
+        double initialX = GameManager.getPlayer().getX();
+        press(KeyCode.RIGHT);
+        double finalX = GameManager.getPlayer().getX();
+        Assert.assertTrue(initialX < finalX);
+    }
+
+    @Test
+    public void testMoveUp(){
+        double initialY = GameManager.getPlayer().getY();
+        press(KeyCode.UP);
+        double finalY = GameManager.getPlayer().getY();
+        Assert.assertTrue(initialY > finalY);
+    }
+
+    @Test
+    public void testMoveDown(){
+        double initialY = GameManager.getPlayer().getY();
+        press(KeyCode.DOWN);
+        double finalY = GameManager.getPlayer().getY();
+        Assert.assertTrue(initialY < finalY);
+    }
+
+
+
+
 }
+
+
