e4f38320f1e6bd8cccbf66fcdab66c1ecda1a07c
Andy Lee <alee435@gatech.edu>
2021-04-15 18:26:32 -0400

    weapon added

--- src/main/java/game/collectables/newWeapon.java
+++ src/main/java/game/collectables/newWeapon.java
@@ -1,2 +1,30 @@
-package game.collectables;public class newWeapon {
+package game.collectables;
+
+import core.SoundManager;
+import game.collidables.Collidable;
+import game.entities.Player;
+import javafx.geometry.Point2D;
+import utilities.RandomUtil;
+
+/**
+ * A coin that the player can collect to increase their money
+ */
+public class newWeapon extends Collectable {
+    /**
+     * Creates an instance of a items placed randomly within the room.
+     */
+    public newWeapon() {
+        super("/images/sword.gif", RandomUtil.getPoint2D(300), new Point2D(2, 2));
+    }
+
+    @Override
+    public void onCollision(Collidable other) {
+        if (isCollected || !(other instanceof Player)) {
+            return;
+        }
+
+        Player.getPlayer().setWeaponObtained();
+        setCollected();
+        SoundManager.playCoinOrKeyCollected();
+    }
 }
