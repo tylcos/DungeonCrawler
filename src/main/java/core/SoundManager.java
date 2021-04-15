90b7d591838f94a8bc8f04cd4c0fca2868200f10
tylcos <tylcos@users.noreply.github.com>
2021-04-15 03:43:13 -0400

    Add volume parameter to the SoundManager

--- src/main/java/core/SoundManager.java
+++ src/main/java/core/SoundManager.java
@@ -4,55 +4,53 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
-import java.io.File;
+import java.nio.file.Paths;
 
-public final class SoundManager { //kinda spotty sometimes, but we can add more as we go
+/**
+ * Manages all sounds playing in the game.
+ *
+ * kinda spotty sometimes, but we can add more as we go.
+ */
+public final class SoundManager {
+    private static double volume = 1d;
 
+    private SoundManager() { }
+
+    private static void playSound(String audioPath, int duration) {
+        MediaPlayer mediaPlayer =
+                new MediaPlayer(new Media(Paths.get(audioPath).toUri().toString()));
+
+        mediaPlayer.setRate(1.5d);
+        mediaPlayer.setVolume(volume);
+        mediaPlayer.play();
+        mediaPlayer.setStopTime(new Duration(duration));
+    }
 
     public static void playDoorCreak() {
-
-        MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("src/main/resources/audio/jail_cell_door.mp3").toURI().toString()));
-        mediaPlayer.setRate(1.5);
-        mediaPlayer.play();
-        mediaPlayer.setStopTime(new Duration(1500));
+        playSound("src/main/resources/audio/jail_cell_door.mp3", 1000);
     }
 
     public static void playPlayerAttacked() {
-        MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("src/main/resources/audio/lose sound 2 - 2.wav").toURI().toString()));
-        mediaPlayer.setRate(1.5);
-        mediaPlayer.setStopTime(new Duration(1000));
-        mediaPlayer.play();
+        playSound("src/main/resources/audio/lose sound 2 - 2.wav", 1000);
     }
+
     public static void playEnemyKilled() {
-        MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("src/main/resources/audio/positive 1.mp3").toURI().toString()));
-        mediaPlayer.setRate(1.5);
-        mediaPlayer.setStopTime(new Duration(1000));
-        mediaPlayer.play();
+        playSound("src/main/resources/audio/positive 1.mp3", 1000);
     }
+
     public static void playCoinOrKeyCollected() {
-        MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("src/main/resources/audio/Coin-collect-sound-effect.mp3").toURI().toString()));
-        mediaPlayer.setRate(1.5);
-        mediaPlayer.setStopTime(new Duration(1000));
-        mediaPlayer.play();
+        playSound("src/main/resources/audio/Coin-collect-sound-effect.mp3", 1000);
     }
+
     public static void playPotionCollected() {
-        MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("src/main/resources/audio/healspell3.aif").toURI().toString()));
-        mediaPlayer.setRate(1.5);
-        mediaPlayer.setStopTime(new Duration(1000));
-        mediaPlayer.play();
+        playSound("src/main/resources/audio/healspell3.aif", 1000);
     }
+
     public static void playKeyActivated() {
-        MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("src/main/resources/audio/key2 pickup.mp3").toURI().toString()));
-        mediaPlayer.setRate(1.5);
-        mediaPlayer.setStopTime(new Duration(1000));
-        mediaPlayer.play();
+        playSound("src/main/resources/audio/key2 pickup.mp3", 1000);
     }
+
     public static void playVictory() {
-        MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("src/main/resources/audio/Old victory sound roblox.mp3").toURI().toString()));
-        mediaPlayer.setRate(1.5);
-        mediaPlayer.setStopTime(new Duration(3000));
-        mediaPlayer.play();
+        playSound("src/main/resources/audio/Old victory sound roblox.mp3", 3000);
     }
-
-
 }
