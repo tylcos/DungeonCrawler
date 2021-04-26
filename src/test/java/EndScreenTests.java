af5168e9bda843189d288c52c1ecbd5775019ed2
kpatell <kpatell@users.noreply.github.com>
2021-04-26 12:41:29 -0400

    add m6 end screen tests

--- src/test/java/EndScreenTests.java
+++ src/test/java/EndScreenTests.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
+import views.EndScreen;
 
 import static org.junit.Assert.assertEquals;
 import static org.testfx.api.FxAssert.verifyThat;
@@ -31,5 +32,31 @@ public class EndScreenTests extends ApplicationTest {
     @Test
     public void testExitVisible() {
         verifyThat("Exit Game", NodeMatchers.isVisible());
+    }
+
+    @Test
+    public void testKillLabelExist(){assertEquals(0, EndScreen.getTotalKill()); }
+
+    @Test
+    public void testPotionLabelExist(){assertEquals(0, EndScreen.getTotalPotionsObtained()); }
+
+    @Test
+    public void testNukeLabelExist(){ assertEquals(0, EndScreen.getTotalNukesUsed());}
+
+    @Test
+    public void testMonsterKillIncreased(){
+        EndScreen.addTotalKilled();
+        assertEquals(EndScreen.getTotalKill(), 1);
+    }
+
+    @Test
+    public void testTotalNukeIncreased(){
+        EndScreen.addTotalNukesUsed();
+        assertEquals(EndScreen.getTotalNukesUsed(),1);
+    }
+    @Test
+    public void testTotalPositionIncreased(){
+        EndScreen.addTotalPotionsObtained();
+        assertEquals(EndScreen.getTotalPotionsObtained(),1);
     }
 }
